4ab01786f12a779bf654eb07cb88c7ee6e9ff8e6
Nohan550 <nohanmuttakim.bd@gmail.com>
2024-06-24 16:38:38 +0600

    footer added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+### live_link = https://apple-15-website-clone.netlify.app/
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,39 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
   return (
     <footer className="py-5 sm:px-10 px-5">
       <div className="screen-max-width">
         <div>
-          <p className="font-semibold text-xs text-gray">More Ways to shop
-            <span className="underline text-blue"></span>
+          <p className="font-semibold text-xs text-gray">
+            More Ways to shop :
+            <span className="underline text-blue">
+              Find an Apple Store {""}
+            </span>
+            or
+            <span className="underline text-blue">Other Retailer {""}</span>
+            near you
+          </p>
+          <p className="font-semibold text-xs text-gray">
+            Or call 000800-040-1455
+          </p>
+        </div>
+        <div className="bg-neutral-700 my-5 h-[1px] " />
+
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-xs text-gray">
+            Copyright @ 2024 Apple Inc. All rights reserved
+            <div className="flex">
+              {footerLinks.map((link, i) => (
+                <p key={link} className="font-semibold text-gray text-xs">
+                  {link}
+                  {""}
+                  {i !== footerLinks.length - 1 && (
+                    <span className="mx-2">|</span>
+                  )}
+                </p>
+              ))}
+            </div>
           </p>
         </div>
       </div>
